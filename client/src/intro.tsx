eff8058f1f7286b88b5403c2f1e3822715e4cc3f
nonyeah <nonyeulasi@hotmail.com>
2025-06-09 22:46:45 +0100

    major changes to location module. Text is now written to the DOM on mount

--- client/src/intro.tsx
+++ client/src/intro.tsx
@@ -1,25 +1,40 @@
+import { useState, useEffect, useRef } from "react";
 
 export default function Intro() {
+  const pictureRef = useRef<null | HTMLDivElement>(null);
+  const [rotate, setrotate] = useState<boolean>(false);
+
+  useEffect(() => {
+    let id: any = setTimeout(() => setrotate(!rotate), 1000);
+    return () => clearTimeout(id);
+  }, []);
+
   return (
     <div className="intro-container">
       <h4>Saturday November 1st 2025</h4>
-      
-      <div className="photo"></div>
+
+      <div
+        className={`photo ${rotate ? "rotate-pic" : ""}`}
+        ref={pictureRef}
+      ></div>
       <h1>Our beloved Mother turns 80 this year!</h1>
 
       <p>To Christ Jesus be the glory!</p>
       <p>
         Come help us celebrate the 80th birthday of our beloved mother,
-        grandmother, sister, aunty, cousin and friend to many, Mrs Ethel Ulasi, who 
-        on 31st October 2025, will have been alive to witness the earth rotate around 
-        the sun 80 times. How amazing is that! We are truly grateful to God and our 
-        Lord Jesus Christ, for blessing our dear mother with
-        such a long and fruitful life and we pray our good God will continue to
-        satisfy her with His everlasting goodness.
+        grandmother, sister, aunty, cousin and friend to many, Mrs Ethel Ulasi,
+        who on 31st October 2025, will have been alive to witness the earth
+        rotate around the sun 80 times. How amazing is that! We are truly
+        grateful to God and our Lord Jesus Christ, for blessing our dear mother
+        with such a long and fruitful life and we pray our good God will
+        continue to satisfy her with His everlasting goodness.
       </p>
       <p>
         Come join us as we celebrate! Let's eat some delicious food, drink until
-        we are merry and <p><span>DANCE, DANCE, DANCE!</span></p>
+        we are merry and{" "}
+        <p>
+          <span>DANCE, DANCE, DANCE!</span>
+        </p>
       </p>
     </div>
   );
--- client/src/location.tsx
+++ client/src/location.tsx
@@ -1,66 +1,169 @@
+import { useState, useEffect, useRef } from "react";
+
+interface Data {
+  id: number;
+  label: string;
+  text: string | string[];
+  counter: number;
+  setcounter: (param: number) => void;
+}
+
+interface EventData {
+  id: number;
+  label: string;
+  text: string | string[];
+}
+
+const data: Array<EventData> = [
+  { id: 0, label: "Date:", text: "Saturday November 1st 2025" },
+  { id: 1, label: "Time:", text: "6pm - 11.30pm" },
+  {
+    id: 2,
+    label: "Where:",
+    text: [
+      "Learie Constantine Centre",
+      "Villiers Road (nearest tube station - Dollis Hill)",
+      "London NW2 2FD",
+    ],
+  },
+  {
+    id: 3,
+    label: "Parking:",
+    text: " An abundance of free parking bays are available on the main road and surrounding roads all day",
+  },
+  {
+    id: 4,
+    label: "Dress Code:",
+    text: "Glam up in your best party attire and come looking Instagram ready!",
+  },
+  {
+    id: 5,
+    label: "Itinerary:",
+    text: [
+      "6pm - 6.30pm: &nbsp; Arrival of guests",
+      "6.45pm - 7pm: &nbsp; Opening prayer and introductions",
+      "7.15pm - 8pm: &nbsp; Family presentation and MC takeover",
+      "8.15pm - 9.15pm: &nbsp; Delicious dinner and cutting of birthday cake",
+      "9.30pm - 11.30pm: &nbsp; Dance floor opens. Let's shake a leg or two to some good grooves",
+    ],
+  },
+  {
+    id: 6,
+    label: "Details:",
+    text: " Please arrive on time so you don't miss out on the good parts of the evening. This is an adult only event so we politely ask that only those directly invited attend. We can of course accommodate adult family members who are accompanying elderly guests.",
+  },
+];
+
+function CreateItinerary({ id, label, text, counter, setcounter }: Data) {
+  const labelRef = useRef<null | HTMLParagraphElement>(null);
+  const textRef = useRef<null | HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let timerId: any;
+      //if react node id matches state variable counter run the effect
+      if (id === counter) {
+        new Promise<string>((resolve: (value: string) => void) => {
+          //obtain each string label value via a promise
+          timerId = setTimeout(() => resolve(label), 4000);
+        })
+          .then((label) => {
+            let i = 0;
+            let intervalId: any = setInterval(() => {
+              //append each character in the label string to the DOM
+              if (i < label.length && labelRef.current) {
+                labelRef.current!.innerHTML += label[i];
+                i++;
+              } else {
+                clearInterval(intervalId);
+              }
+            }, 200);
+            return text;
+          })
+          .then((textvalue) => {
+            setTimeout(() => {
+              let n = 0;
+              let intervalId: any;
+              if (typeof textvalue === "string") {
+                const arr: string[] = textvalue.split(" ");
+                intervalId = setInterval(() => {
+                  if (n < arr.length) {
+                    textRef.current!.innerHTML += `${arr[n]} `;
+                    n++;
+                  } else {
+                    clearInterval(intervalId);
+                  }
+                }, 200);
+                setcounter(counter + 1);
+              } else if (Array.isArray(textvalue)) {
+                let intervalId: any = setInterval(() => {
+                  if (n < textvalue.length) {
+                    textRef.current!.innerHTML += `${textvalue[n]} <br>`;
+                    n++;
+                  } else {
+                    clearInterval(intervalId);
+                  }
+                }, 200);
+                setcounter(counter + 1);
+              }
+            }, 2000);
+          });
+      }
+      return () => clearInterval(timerId);
+    }, id);
+  }, [counter]);
+
+  return (
+    <div className="itin-inner">
+      <p className="left" ref={labelRef}></p>
+      <p className="right" ref={textRef}></p>
+    </div>
+  );
+}
+
 export default function Address() {
+  const titleRef = useRef<null | HTMLHeadingElement>(null);
+  const titleText = "Where we plan to party?";
+  const [count, setcounter] = useState<number>(0);
+
+  const details = data.map((eventinfo) => {
+    return (
+      <CreateItinerary
+        key={eventinfo.id}
+        id={eventinfo.id}
+        label={eventinfo.label}
+        text={eventinfo.text}
+        counter={count}
+        setcounter={setcounter}
+      />
+    );
+  });
+
+  useEffect(() => {
+    let i = 200;
+    let timeouts: any[] = [];
+
+    if (titleRef.current) {
+      titleRef.current.innerHTML = ""; // Clear previous runs
+    }
+
+    for (let char of titleText) {
+      const timeoutId = setTimeout(() => {
+        titleRef.current!.innerHTML += char;
+      }, 1000 + (i += 50));
+      timeouts.push(timeoutId);
+    }
+
+    return () => {
+      timeouts.forEach(clearTimeout);
+    };
+  }, []);
+
   return (
     <div className="address-container">
-      <h2>Where We Plan To Party?</h2>
+      <h2 ref={titleRef}></h2>
 
-      <div className="address-details">
-
-        <div className="time">
-          <p className="left">Date:</p>
-          <p className="right">Saturday November 1st 2025</p>
-        </div>
-        
-        <div className="time">
-          <p className="left">Time:</p>
-          <p className="right">6pm - 11.30pm</p>
-        </div>
-
-        <div className="address">
-          <p className="left">Where:</p>
-          <section className="right">
-           <p> Learie Constantine Centre</p> 
-            <p className="inner-paragraph">Villiers Road (nearest tube station - Dollis Hill)</p>
-            <p className="inner-paragraph"> London NW2 2FD</p>
-          </section>
-        </div>
-
-        <div className="parking">
-          <p className="left">Parking:</p>
-          <p className="right">An abundance of free parking bays are available on the main road and surrounding 
-            roads all day
-          </p>
-        </div>
-
-        <div className="dress-code">
-          <p className="left">Dress Code:</p>
-          <p className="right">Glam up in your best party attire and come looking Instagram ready!
-          </p>
-        </div>
-
-        <div className="itenary">
-          <p className="left">Itinerary:</p>
-          <p className="right">
-            <ul>
-              <li>6pm - 6.30pm: &nbsp; Arrival of guests</li>
-              <li>6.45pm - 7pm: &nbsp; Opening prayer and introductions</li>
-              <li>7.15pm - 8pm: &nbsp; Family presentation and MC takeover</li>
-              <li>8.15pm - 9.15pm: &nbsp; Delicious dinner and cutting of birthday cake</li>
-              <li>9.30pm - 11.30pm: &nbsp; Dance floor opens. Let's shake a leg or two to some good grooves</li>
-            </ul>
-          </p>
-        </div>
-
-        <div className="details">
-            <p className="left">Details:</p>
-          <p className="right">
-            Please arrive on time so you don't miss out on the good parts
-            of the evening. This is an <b>adult only event</b> so we politely ask that  
-            only those directly invited attend. We can of course accommodate adult 
-            family members who are accompanying elderly guests.
-          </p>
-        </div>
-     
-      </div>
+      <div className="address-details">{details}</div>
     </div>
   );
 }
